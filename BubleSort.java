a92d1c663cc54bbd2d4ce2d6bf4b425b74a483de
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-18 21:41:47 -0600

    Construcción del algoritmo BubleSort

--- BubleSort.java
+++ BubleSort.java
@@ -57,4 +57,27 @@ public class BubleSort {
             return null;
         }
     }
+
+     // Construcción del algoritmo BubbleSort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean intercambio;
+
+        for (int i = 0; i < n - 1; i++) {
+            intercambio = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    // El intercambio de valores
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    intercambio = true;
+                }
+            }
+
+            if (!intercambio) break; // Ojo! si no hubo intercambio, el array ya está ordenado
+        }
+    }
 }
