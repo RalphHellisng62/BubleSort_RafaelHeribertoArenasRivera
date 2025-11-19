cf5be0d2ec0da0577db93a54174579ccae515b73
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-18 21:32:12 -0600

    Se creo el main

--- BubleSort.java
+++ BubleSort.java
@@ -1,3 +1,25 @@
 public class BubleSort {
-    
+    // Iniciamos con la construcción de un main
+
+    public static void main(String[] args) {
+    String archivoEntrada = "numerosBubleSort.txt";
+    String archivoSalida = "numerosBubleSort_ordenados.txt";
+
+        int[] numeros = leerArchivo(archivoEntrada);
+
+        if (numeros != null) {
+            System.out.println("numeros originales:");
+            imprimirArray(numeros);
+
+            bubbleSort(numeros);
+
+            System.out.println("\n numeros ordenados (Bubble Sort):");
+            imprimirArray(numeros);
+
+            escribirArchivo(numeros, archivoSalida);
+            System.out.println("\n Archivo generado exitosamente: " + archivoSalida);
+        } else {
+            System.out.println("No se pudo leer el archivo.");
+        }  
+    }
 }
